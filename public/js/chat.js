5d7da6a5081d00844cff5e145129b1b744a022cd
Chayan <chayanbansal1999@gmail.com>
2019-09-01 13:35:19 +0530

    final added backend of chat

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,31 +1,24 @@
-var socket = io.connect("http://localhost:3000")
+var socket = io.connect("http://localhost:3000");
 
+$(function() {
+  var socket = io();
 
-    $(function () {
-        var socket = io();
-        $('form').submit(function(e){
-          e.preventDefault(); // prevents page reloading
-          socket.emit('chat message', $('#m').val());
-          $('#m').val('');
-          return false;
-        });
-        socket.on('chat message', function(msg){
-            $('#messages').append($('<li>').text(msg));
-        });
-      });
-
-      //audio
-      function select(){
-      $(function () {
-        var socket = io();
-        $('form').submit(function(e){
-          e.preventDefault(); // prevents page reloading
-          socket.emit('chat message', $('#mu').val());
-          $('#mu').val('');
-          return false;
-        });
-        socket.on('chat message', function(msg){
-           $('#messages').append($('<li><audio controls src="audio/mu" type="audio/mpeg"></audio>'));
-        });
-      });
+ 
+  $("form").submit(function(e) {
+    e.preventDefault(); // prevents page reloading
+    socket.emit("chat message", $("#mu").val());
+    $("#mu").val("");
+    return false;
+  });
+  socket.on("chat message", function(msg) {
+    console.log(msg);
+    if (msg.indexOf(".") !== -1) {
+      $("#messages").append(
+        $(`<li><audio controls src="/audio/${msg}" type="audio/mpeg"></audio>`)
+      );
+    } else {
+      $("#messages").append($(`<li>`).text(msg));
     }
+  });
+});
+
